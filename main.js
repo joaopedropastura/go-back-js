0b7b3e5b0aa091e03e576a3ff60351b7c10c8cbf
Joao Pedro Soares <jpsoares111@hotmail.com>
2022-07-13 21:56:44 -0300

    tentando criar a matriz

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const snake = document.getElementById("snake");
+const map = document.getElementById("map");
+addEventListener("keypress", movement);
+
+let win = false;
+let positionSnake = [];
 function movement(event)
 {
 	var topVal = parseInt(window.getComputedStyle(snake).top, 10);
 	let leftVal = parseInt(window.getComputedStyle(snake).left, 10);
-	console.log("alo")
+	console.log("alo");
 	let pressed = event.charCode;
 	if(pressed == 83 || pressed == 115) // numeros representando a letra (S && s)
 		snake.style.top = (topVal + 16) + "px";
@@ -15,8 +20,21 @@ function movement(event)
 		snake.style.top = (topVal - 16) + "px";
 
 }
+
+function drawMap()
+{
+	for(let i = 0; i <= 15; i++)
+	{
+		let divColumn = document.createElement("div");
+		divColumn.className = "pixel";
+		divColumn.addEventListener("keypress", )
+		for(let j = 0; j <= 15; j++)
+		{
+
+		}
+	}
+}
 // function getClick()
 // {
-	addEventListener("keypress", movement)
 // }
 // document.addEventListener("DOMContentLoaded", getClick)
